b4bf3f3eb5bbff967073a8c422f434a4012eabce
kevinoctavian <kebimbangan12@gmail.com>
2022-04-11 13:27:14 +0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = __importDefault(require("./routes"));
+const morgan_1 = __importDefault(require("morgan"));
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const web_1 = __importDefault(require("./routes/web"));
+const PORT = process.env.PORT || 5000;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use((0, morgan_1.default)("dev"));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.set("view engine", "pug");
+app.use("/", web_1.default);
+app.use("/api", routes_1.default);
+app.listen(PORT, () => console.log("Server is running in PORT: " + PORT, process.cpuUsage()));
